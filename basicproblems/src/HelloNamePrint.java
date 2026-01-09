c78612197f358fb9c080d05e07f8e91c5e41438c
Neyamul Adnan <muhammadmojiburrahaman@gmail.com>
2026-01-09 20:07:30 +0600

    initial

---
+++ basicproblems/src/HelloNamePrint.java
@@ -0,0 +1,9 @@
+package com.basicproblems;
+
+public class HelloNamePrint{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Adnan");
+        
+    }
+}
